1d6d47a6a7bd93f21e44ff2caf7b93dca985d717
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-07 13:02:35 -0300

    feat: create User model

---
+++ modules/user/models/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from modules.shared.config.model.base import Base
+from modules.study_trail.models.study_trail import StudyTrail
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250))
+    password = Column(String(250))
+    study_trails = relationship('StudyTrail')
+
+    def __init__(self, email: str, password: str):
+        self.email = email
+        self.password = password
+
+
+    def add_study_trail(self, study_trail: StudyTrail):
+        self.study_trails.append(study_trail)
