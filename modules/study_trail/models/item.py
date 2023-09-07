da9c55e4f0c0f42befd2cdc88f3d85a269b5e32b
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-07 13:03:44 -0300

    feat: create Item model

---
+++ modules/study_trail/models/item.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, Integer, ForeignKey
+from modules.shared.config.model.base import Base
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250))
+    type = Column(String(250))
+    description = Column(String(500))
+    resource = Column(String(500))
+    study_trail = Column(Integer, ForeignKey('study_trail.id'), nullable=False)
+
+    def __init__(self, title: str, type: str, description: str, resource: str):
+        self.title = title
+        self.type = type
+        self.description = description
+        self.resource = resource
