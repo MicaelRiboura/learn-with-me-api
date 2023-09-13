ffadc7f6ec1025b58b80ed761e4018a2e7763225
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-13 19:21:49 -0300

    fix(user_dao_alchemy): adjust find_by_email and create find_by_id

--- modules/user/daos/user_dao_alchemy.py
+++ modules/user/daos/user_dao_alchemy.py
@@ -1,6 +1,7 @@
 from .abstract_user_dao import AbstractUserDAO
 from modules.user.models import User
 from modules.shared.config.db_sqlite import Session
+from modules.shared.config.model.serializer import Serializer
 
 class UserDAO(AbstractUserDAO):
     def create(self, form):
@@ -21,5 +22,17 @@ class UserDAO(AbstractUserDAO):
         session = Session()
 
         user = session.query(User).filter(User.email == email).first()
+
+        if not user:
+            return None
+
+        user_serialized = user.serialize()
+        
+        return user_serialized
+
+    def find_by_id(self, id):
+        session = Session()
+
+        user = session.query(User).filter(User.id == id).first()
 
         return user
