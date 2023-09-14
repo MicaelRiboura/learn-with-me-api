d1ee59c61c4804b821339e95f73e0b85665f7254
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-14 20:21:37 -0300

    feat(study_trail_dao_alchemy): implement list methods

--- modules/study_trail/daos/study_trail/study_trail_dao_alchemy.py
+++ modules/study_trail/daos/study_trail/study_trail_dao_alchemy.py
@@ -16,19 +16,39 @@ class StudyTrailDAO(AbstractStudyTrailDAO):
 
         return user_serialized['study_trails'][len(user_serialized['study_trails']) - 1]
     
-    def find_all(self):
-        return
+    def find_all(self, session=Session()):
+        study_trails = session.query(StudyTrail).all()
+        print('study_trails: ', study_trails)
+
+        study_trails_serialized = []
+        if study_trails:
+            for study_trail in study_trails:
+                study_trails_serialized.append(study_trail.serialize())
+        
+        print('study_trails_serialized: ', study_trails_serialized)
+
+        return study_trails_serialized
     
-    def find_by_title(self, title):
-        return
+    def find_by_title(self, title, session = Session()):
+        study_trails = session.query(StudyTrail).filter(StudyTrail.title == title)
+        study_trails_serialized = []
+        if study_trails:
+            for study_trail in study_trails:
+                study_trails_serialized.append(study_trail.serialize())
+        return study_trails_serialized
     
-    def find_by_user(self, user):
-        return
+    def find_by_user(self, user, session = Session()):
+        study_trails = session.query(StudyTrail).filter(StudyTrail.user == user)
+        study_trails_serialized = []
+        if study_trails:
+            for study_trail in study_trails:
+                study_trails_serialized.append(study_trail.serialize())
+        return study_trails_serialized
     
-    def find_by_id(self, id, session):
+    def find_by_id(self, id, session = Session()):
         study_trail = session.query(StudyTrail).filter(StudyTrail.id == id).first()
 
         return study_trail
     
-    def delete(self, id):
+    def delete(self, id, session = Session()):
         return
